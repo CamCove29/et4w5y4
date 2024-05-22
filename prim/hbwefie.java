9274b9bf70530ae97611aca3c99857de5927a6ab
Camila <111703202+CamCove29@users.noreply.github.com>
2024-05-21 19:30:21 -0500

    jhj

---
+++ prim/hbwefie.java
@@ -0,0 +1,21 @@
+package p
+        SE PEUDE EDIDTAR
+        OEJFVNWRVFWRPFW
+                DVFWRVWRVWRV
+        WFVW
+                        RVW
+        VW
+                                V
+        EW
+                                        V
+        EWF
+
+                                                EWF
+        EWF
+
+                                                        WE
+        FWE
+                                                                F
+        EW
+                                                                        FWDW
+        F
---
+++ prim/wropgkerothtr.java
@@ -0,0 +1,19 @@
+package prim;
+
+public class wropgkerothtr {
+    fjejgiwerjgirt4moir
+
+    goierjgoirwkkg
+            ijejiowrejgnrtko
+}
+ERROFJOER
+
+WEF
+        EC
+DA
+                CA
+
+skdfwk
+                        ksdnc
+ksncks
+                                
